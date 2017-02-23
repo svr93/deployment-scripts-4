5b0f11f02e37156d9551f6b22102b62032c909a9
shamsheev <svr93@i.ua>
2017-02-23 18:03:54 +0300

    NE add base.sh

---
+++ base.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# default project name
+projectName='__new-project__'
+
+while [[ "$1" != '' ]]; do
+    case $1 in
+        -name )                     shift
+                                    projectName=$1
+                                    ;;
+        * )                         echo 'Error: bad param'
+                                    ;;
+    esac
+    shift
+done
+
+projectDir='../'${projectName}
+mkdir ${projectDir}
